f674dd4ae7be89f10d98eea734e37b65a6b68185
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-11-16 21:14:04 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
+// Components
+import Banner from "./components/banner/banner";
+import Info from "./components/info/info";
+
+// Data
+import infoData from "./data/infoData";
+
 function App() {
   return (
     <div className="App">
-      <p>Hi there!</p>
+      <Banner />
+      <div className="container">
+        {infoData.map((info) => (
+          <Info key={info.title} {...info} />
+        ))}
+      </div>
     </div>
   );
 }
